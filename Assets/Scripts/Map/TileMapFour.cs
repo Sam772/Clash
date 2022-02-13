90b7cbbb764c0e338ff303fbc6f2fa8229a3c468
Sam772 <78389553+Sam772@users.noreply.github.com>
2022-02-13 20:41:47 +0000

    MapFour
    Updated map design of MapFour by adding a stone building in the middle of the map. Updated the captain unit sprite.

--- Assets/Scripts/Map/TileMapFour.cs
+++ Assets/Scripts/Map/TileMapFour.cs
@@ -12,8 +12,56 @@ public class TileMapFour : GenericTileMap {
 
         // snow tree tiles
         tiles[3, 8] = 1;
-        tiles[14, 5] = 1;
-        tiles[11, 11] = 1;
-        tiles[6, 13] = 1;
+        tiles[18, 5] = 1;
+        tiles[7, 11] = 1;
+        tiles[19, 13] = 1;
+
+        // stone tiles
+        tiles[10, 2] = 2;
+        tiles[11, 2] = 2;
+        tiles[12, 2] = 2;
+        tiles[13, 2] = 2;
+        tiles[14, 2] = 2;
+        tiles[9, 3] = 2;
+        tiles[15, 3] = 2;
+        tiles[16, 4] = 2;
+        tiles[16, 5] = 2;
+        tiles[16, 6] = 2;
+        tiles[16, 7] = 2;
+        tiles[16, 8] = 2;
+        tiles[16, 9] = 2;
+        tiles[16, 10] = 2;
+        tiles[8, 4] = 2;
+        tiles[8, 5] = 2;
+        tiles[8, 6] = 2;
+        tiles[8, 7] = 2;
+        tiles[8, 8] = 2;
+        tiles[8, 9] = 2;
+        tiles[8, 10] = 2;
+        tiles[9, 11] = 2;
+        tiles[15, 11] = 2;
+        tiles[10, 12] = 2;
+        tiles[11, 12] = 2;
+        tiles[12, 12] = 2;
+        tiles[13, 12] = 2;
+        tiles[14, 12] = 2;
+
+        // stone floor tiles
+        for (int x = 9; x < 16; x++) {
+            for (int y = 4; y < 11; y++) { 
+                tiles[x, y] = 3; 
+            }
+        }
+
+        tiles[10, 3] = 3;
+        tiles[11, 3] = 3;
+        tiles[12, 3] = 3;
+        tiles[13, 3] = 3;
+        tiles[14, 3] = 3;
+        tiles[10, 11] = 3;
+        tiles[11, 11] = 3;
+        tiles[12, 11] = 3;
+        tiles[13, 11] = 3;
+        tiles[14, 11] = 3;
     }
 }
