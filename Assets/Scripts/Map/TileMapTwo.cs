6ff54a0f6dbc4277ae85f16d37e59484247fa182
Sam772 <78389553+Sam772@users.noreply.github.com>
2022-02-15 19:42:39 +0000

    MapTwo
    Added mountain terrain textures to the background of MapTwo, a mountain tile for MapTwo, and
    added descriptions to the map info boxes of each map.

--- Assets/Scripts/Map/TileMapTwo.cs
+++ Assets/Scripts/Map/TileMapTwo.cs
@@ -9,5 +9,17 @@ public class TileMapTwo : GenericTileMap {
         for (int x = 0; x < mapSizeX; x++) {
             for (int y = 0; y < mapSizeY; y++) { tiles[x, y] = 0; }
         }
+
+        // mountain tiles
+        tiles[11, 6] = 1;
+        tiles[11, 7] = 1;
+        tiles[11, 8] = 1;
+        tiles[12, 6] = 1;
+        tiles[12, 7] = 1;
+        tiles[12, 8] = 1;
+        tiles[13, 6] = 1;
+        tiles[13, 7] = 1;
+        tiles[13, 8] = 1;
     }
+
 }
