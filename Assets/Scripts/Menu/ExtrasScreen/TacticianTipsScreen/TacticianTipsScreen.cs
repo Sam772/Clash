220a13493e75123ac8aed2b3a353b89a419d6ae6
m-shah02 <mansoorshah488@gmail.com>
2022-03-23 23:00:01 +0000

    TacticiansTips Screen menu arranged
    Menus and buttons are in place (button layout subject to change) content is placeholder text for the time being

--- Assets/Scripts/Menu/ExtrasScreen/TacticianTipsScreen/TacticianTipsScreen.cs
+++ Assets/Scripts/Menu/ExtrasScreen/TacticianTipsScreen/TacticianTipsScreen.cs
@@ -1,3 +1,98 @@
-public class TacticianTipsScreen : MenuScreen {
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+using TMPro;
+using Mirror;
+using UnityEngine.UI;
 
-}
+public class TacticianTipsScreen : MenuScreen {
+    [SerializeField] private GameObject movementinfo;
+    [SerializeField] private GameObject turnorderinfo;
+    [SerializeField] private GameObject howtowininfo;
+    [SerializeField] private GameObject unitsinfo;
+    [SerializeField] private GameObject tilesinfo;
+    [SerializeField] private GameObject terraininfo;
+    [SerializeField] private GameObject skillsinfo;
+
+    void Start(){
+        movementinfo.SetActive(true);
+        turnorderinfo.SetActive(false);
+        howtowininfo.SetActive(false);
+        unitsinfo.SetActive(false);
+        tilesinfo.SetActive(false);
+        terraininfo.SetActive(false);
+        skillsinfo.SetActive(false);
+    }
+
+    public void ShowMovementInfo(){
+        movementinfo.SetActive(true);
+        turnorderinfo.SetActive(false);
+        howtowininfo.SetActive(false);
+        unitsinfo.SetActive(false);
+        tilesinfo.SetActive(false);
+        terraininfo.SetActive(false);
+        skillsinfo.SetActive(false); 
+    }
+
+    public void ShowTurnOrderInfo(){
+        movementinfo.SetActive(false);
+        turnorderinfo.SetActive(true);
+        howtowininfo.SetActive(false);
+        unitsinfo.SetActive(false);
+        tilesinfo.SetActive(false);
+        terraininfo.SetActive(false);
+        skillsinfo.SetActive(false); 
+    }
+
+    public void ShowHowToWinInfo(){
+        movementinfo.SetActive(false);
+        turnorderinfo.SetActive(false);
+        howtowininfo.SetActive(true);
+        unitsinfo.SetActive(false);
+        tilesinfo.SetActive(false);
+        terraininfo.SetActive(false);
+        skillsinfo.SetActive(false); 
+    }
+
+    public void ShowUnitsInfo(){
+        movementinfo.SetActive(false);
+        turnorderinfo.SetActive(false);
+        howtowininfo.SetActive(false);
+        unitsinfo.SetActive(true);
+        tilesinfo.SetActive(false);
+        terraininfo.SetActive(false);
+        skillsinfo.SetActive(false); 
+    }
+
+    public void ShowTilesInfo(){
+        movementinfo.SetActive(false);
+        turnorderinfo.SetActive(false);
+        howtowininfo.SetActive(false);
+        unitsinfo.SetActive(false);
+        tilesinfo.SetActive(true);
+        terraininfo.SetActive(false);
+        skillsinfo.SetActive(false); 
+    }
+
+    public void ShowTerrainInfo(){
+        movementinfo.SetActive(false);
+        turnorderinfo.SetActive(false);
+        howtowininfo.SetActive(false);
+        unitsinfo.SetActive(false);
+        tilesinfo.SetActive(false);
+        terraininfo.SetActive(true);
+        skillsinfo.SetActive(false); 
+    }
+
+    public void ShowSkillsInfo(){
+        movementinfo.SetActive(false);
+        turnorderinfo.SetActive(false);
+        howtowininfo.SetActive(false);
+        unitsinfo.SetActive(false);
+        tilesinfo.SetActive(false);
+        terraininfo.SetActive(false);
+        skillsinfo.SetActive(true); 
+    }
+
+}  
