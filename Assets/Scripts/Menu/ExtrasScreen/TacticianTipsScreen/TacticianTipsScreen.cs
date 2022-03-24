754922f695f34aa0df8faa620613bd2c49e7ee33
m-shah02 <mansoorshah488@gmail.com>
2022-03-24 14:58:29 +0000

    Restructured TT screen + added skills to UI screen
    Tacticians Tips now has a restructured look with buttons on a scroll wheel instead of having a set of 7 buttons. Button order was also shifted around along with 3 new buttons.
    Unit Info has additional skills written down for each unit too

--- Assets/Scripts/Menu/ExtrasScreen/TacticianTipsScreen/TacticianTipsScreen.cs
+++ Assets/Scripts/Menu/ExtrasScreen/TacticianTipsScreen/TacticianTipsScreen.cs
@@ -7,92 +7,160 @@ using Mirror;
 using UnityEngine.UI;
 
 public class TacticianTipsScreen : MenuScreen {
+    [SerializeField] private GameObject unitsinfo;
     [SerializeField] private GameObject movementinfo;
     [SerializeField] private GameObject turnorderinfo;
-    [SerializeField] private GameObject howtowininfo;
-    [SerializeField] private GameObject unitsinfo;
-    [SerializeField] private GameObject tilesinfo;
+    [SerializeField] private GameObject unitstatsinfo;
+    [SerializeField] private GameObject attackinginfo;
     [SerializeField] private GameObject terraininfo;
     [SerializeField] private GameObject skillsinfo;
+    [SerializeField] private GameObject tilesinfo;
+    [SerializeField] private GameObject camerainfo;
+    [SerializeField] private GameObject howtowininfo;
+    
+        
 
     void Start(){
-        movementinfo.SetActive(true);
+        unitsinfo.SetActive(true);
+        movementinfo.SetActive(false);
         turnorderinfo.SetActive(false);
-        howtowininfo.SetActive(false);
-        unitsinfo.SetActive(false);
-        tilesinfo.SetActive(false);
+        unitstatsinfo.SetActive(false);
+        attackinginfo.SetActive(false);
         terraininfo.SetActive(false);
         skillsinfo.SetActive(false);
-    }
-
-    public void ShowMovementInfo(){
-        movementinfo.SetActive(true);
-        turnorderinfo.SetActive(false);
+        tilesinfo.SetActive(false);
+        camerainfo.SetActive(false);
         howtowininfo.SetActive(false);
-        unitsinfo.SetActive(false);
-        tilesinfo.SetActive(false);
-        terraininfo.SetActive(false);
-        skillsinfo.SetActive(false); 
-    }
 
-    public void ShowTurnOrderInfo(){
-        movementinfo.SetActive(false);
-        turnorderinfo.SetActive(true);
-        howtowininfo.SetActive(false);
-        unitsinfo.SetActive(false);
-        tilesinfo.SetActive(false);
-        terraininfo.SetActive(false);
-        skillsinfo.SetActive(false); 
-    }
-
-    public void ShowHowToWinInfo(){
-        movementinfo.SetActive(false);
-        turnorderinfo.SetActive(false);
-        howtowininfo.SetActive(true);
-        unitsinfo.SetActive(false);
-        tilesinfo.SetActive(false);
-        terraininfo.SetActive(false);
-        skillsinfo.SetActive(false); 
     }
 
     public void ShowUnitsInfo(){
+        unitsinfo.SetActive(true);
         movementinfo.SetActive(false);
         turnorderinfo.SetActive(false);
-        howtowininfo.SetActive(false);
-        unitsinfo.SetActive(true);
-        tilesinfo.SetActive(false);
+        unitstatsinfo.SetActive(false);
+        attackinginfo.SetActive(false);
         terraininfo.SetActive(false);
-        skillsinfo.SetActive(false); 
+        skillsinfo.SetActive(false);
+        tilesinfo.SetActive(false);
+        camerainfo.SetActive(false);
+        howtowininfo.SetActive(false);        
+    }
+   
+    public void ShowMovementInfo(){
+        unitsinfo.SetActive(false);
+        movementinfo.SetActive(true);
+        turnorderinfo.SetActive(false);
+        unitstatsinfo.SetActive(false);
+        attackinginfo.SetActive(false);
+        terraininfo.SetActive(false);
+        skillsinfo.SetActive(false);
+        tilesinfo.SetActive(false);
+        camerainfo.SetActive(false);
+        howtowininfo.SetActive(false);        
     }
 
-    public void ShowTilesInfo(){
+    public void ShowTurnOrderInfo(){
+        unitsinfo.SetActive(false);
+        movementinfo.SetActive(false);
+        turnorderinfo.SetActive(true);
+        unitstatsinfo.SetActive(false);
+        attackinginfo.SetActive(false);
+        terraininfo.SetActive(false);
+        skillsinfo.SetActive(false);
+        tilesinfo.SetActive(false);
+        camerainfo.SetActive(false);
+        howtowininfo.SetActive(false);        
+    }
+
+    public void ShowUnitStatsInfo(){
+        unitsinfo.SetActive(false);
         movementinfo.SetActive(false);
         turnorderinfo.SetActive(false);
-        howtowininfo.SetActive(false);
-        unitsinfo.SetActive(false);
-        tilesinfo.SetActive(true);
+        unitstatsinfo.SetActive(true);
+        attackinginfo.SetActive(false);
         terraininfo.SetActive(false);
-        skillsinfo.SetActive(false); 
+        skillsinfo.SetActive(false);
+        tilesinfo.SetActive(false);
+        camerainfo.SetActive(false);
+        howtowininfo.SetActive(false);        
+    }
+
+    public void ShowAttackingInfo(){
+        unitsinfo.SetActive(false);
+        movementinfo.SetActive(false);
+        turnorderinfo.SetActive(false);
+        unitstatsinfo.SetActive(false);
+        attackinginfo.SetActive(true);
+        terraininfo.SetActive(false);
+        skillsinfo.SetActive(false);
+        tilesinfo.SetActive(false);
+        camerainfo.SetActive(false);
+        howtowininfo.SetActive(false);        
     }
 
     public void ShowTerrainInfo(){
+        unitsinfo.SetActive(false);
         movementinfo.SetActive(false);
         turnorderinfo.SetActive(false);
-        howtowininfo.SetActive(false);
-        unitsinfo.SetActive(false);
-        tilesinfo.SetActive(false);
+        unitstatsinfo.SetActive(false);
+        attackinginfo.SetActive(false);
         terraininfo.SetActive(true);
-        skillsinfo.SetActive(false); 
+        skillsinfo.SetActive(false);
+        tilesinfo.SetActive(false);
+        camerainfo.SetActive(false);
+        howtowininfo.SetActive(false);        
     }
 
     public void ShowSkillsInfo(){
+        unitsinfo.SetActive(false);
         movementinfo.SetActive(false);
         turnorderinfo.SetActive(false);
-        howtowininfo.SetActive(false);
-        unitsinfo.SetActive(false);
-        tilesinfo.SetActive(false);
+        unitstatsinfo.SetActive(false);
+        attackinginfo.SetActive(false);
         terraininfo.SetActive(false);
-        skillsinfo.SetActive(true); 
+        skillsinfo.SetActive(true);
+        tilesinfo.SetActive(false);
+        camerainfo.SetActive(false);
+        howtowininfo.SetActive(false);        
     }
 
+    public void ShowTilesInfo(){
+        unitsinfo.SetActive(false);
+        movementinfo.SetActive(false);
+        turnorderinfo.SetActive(false);
+        unitstatsinfo.SetActive(false);
+        attackinginfo.SetActive(false);
+        terraininfo.SetActive(false);
+        skillsinfo.SetActive(false);
+        tilesinfo.SetActive(true);
+        camerainfo.SetActive(false);
+        howtowininfo.SetActive(false);        
+    }
+
+    public void ShowCameraInfo(){
+        unitsinfo.SetActive(false);
+        movementinfo.SetActive(false);
+        turnorderinfo.SetActive(false);
+        unitstatsinfo.SetActive(false);
+        attackinginfo.SetActive(false);
+        terraininfo.SetActive(false);
+        skillsinfo.SetActive(false);
+        tilesinfo.SetActive(false);
+        camerainfo.SetActive(true);
+        howtowininfo.SetActive(false);        
+    }
+
+    public void ShowHowToWinInfo(){
+        unitsinfo.SetActive(false);
+        movementinfo.SetActive(false);
+        turnorderinfo.SetActive(false);
+        unitstatsinfo.SetActive(false);
+        attackinginfo.SetActive(false);
+        terraininfo.SetActive(false);
+        skillsinfo.SetActive(false);
+        tilesinfo.SetActive(false);
+        camerainfo.SetActive(false);
+        howtowininfo.SetActive(true);        
+    }
 }  
